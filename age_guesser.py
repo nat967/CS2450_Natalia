5f805d2c741053bc00c0693657f2a2ef0cd8ab32
Natalia <natalia@Natalias-MacBook-Air.local>
2026-01-20 10:03:07 -0700

    first commit

---
+++ age_guesser.py
@@ -0,0 +1,16 @@
+import random
+
+print("Hi! I'm going to try to guess your age.")
+
+name = input("What's your name? ")
+
+while True:
+    age_guess = random.randint(15, 30)
+    answer = input(f"Is your age {age_guess}? (y/n): ").strip().lower()
+
+    if answer == "y":
+        print(f"{name} is {age_guess} years old.")
+        break
+    else:
+        print("Rats.")
+
